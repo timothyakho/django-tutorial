dbfec6d98b83bae9a8a4ad4adace77445576b569
Syntactic Sugar <timothyakho@gmail.com>
2016-07-24 16:15:41 -0400

    Added test cases from part 5 of tutorial

--- polls/views.py
+++ polls/views.py
@@ -3,26 +3,35 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.core.urlresolvers import reverse
 from .models import Question, Choice
+from django.utils import timezone
+
+from django.views import generic
 
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = { 
-            "latest_question_list": latest_question_list,
-            }
-    return HttpResponse(template.render(context, request))
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist.")
-    return render(request, "polls/detail.html", {"question": question})
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question" : question})
+    def get_queryset(self):
+        #Return last five questions
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+
+class DetailView(generic.DetailView):
+    template_name = "polls/detail.html"
+    model = Question
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+class ResultsView(generic.DetailView):
+    template_name = "polls/results.html"
+    model = Question
+
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
