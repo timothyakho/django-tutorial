1d263c67bdce3c1892128c35c1ad05222acfe2b3
Syntactic Sugar <timothyakho@gmail.com>
2016-07-23 23:23:41 -0400

    Added database update to poll submission view (vote)

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
-from .models import Question
+from django.core.urlresolvers import reverse
+from .models import Question, Choice
 
 # Create your views here.
 def index(request):
@@ -24,5 +25,13 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {"question": question, "error_message", "You didn't select a choice."})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
